ae3344df38c82acb062600e964a1f1f1bce46302
Emmanuel Gardiner <egard001@gold.ac.uk>
2020-11-06 19:03:44 +0000

    Scripting
    Began work on movement mechanic

---
+++ Year_3_Game/Assets/ClickToMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour
+{
+    [SerializeField]
+    [Range(2, 12)]
+    private float speed = 4;
+
+    private Vector3 targetPosition;
+    private bool isMoving = true;
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            setTargetPosition();
+        }
+
+        if(isMoving)
+        {
+            move();
+        }
+    }
+
+    void setTargetPosition()
+    {
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition.z = transform.position.z;
+
+        isMoving = true;
+    }
+
+    void move()
+    {
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if(transform.position == targetPosition)
+        {
+            isMoving = false;
+        }
+    }
+}
